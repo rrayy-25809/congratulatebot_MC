f0d4a421bd42fd459e1fcad6bfa2554d0f67146c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2022-09-23 23:14:05 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const mineflayer = require('mineflayer')
+var address = "localhost";	//서버 주소
+var bots = [];
+var server_port = 25565;    //서버포트(기본:25565)
+
+function createBot() {
+	let bot = mineflayer.createBot({
+		host: address,
+		username: `bot_${bots.length}`,
+		viewDistance: "tiny",
+        port: server_port
+	});
+	return(bot);
+}
+
+function ready() {
+	bots.push(createBot());
+	if (bots.length < 5) {
+		setTimeout(ready, 10000);
+	} else {
+		bots[0].chat("Ready!");
+		bots[0].on('spawn',chat);
+	}
+}
+
+function chat(username) {
+	do {
+		bots[i].chat("Happy birthday!"+username)
+		i += 1;
+	  } while (i < 5);
+}
+ready();
